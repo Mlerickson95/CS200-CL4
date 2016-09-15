366f6ca982c18747e0429e40c0dd8bc54b21d6d0
Mlerickson95 <mlerickson95@gmail.com>
2016-09-15 18:49:51 -0500

    Add variables.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+	bool done = false;
+	const int foodConsumed_PerPersonPerDay = 2;
+	const int oxygenCreated_PerDay = 5;
+	const int wasteCreated_PerPersonPerDay = 3;
+	const int oxygenUsed_PerPersonPerDay = 2;
+	int foodGrown_PerDay;
+	int cleanup_PerPersonPerDay;
+	int foodCount;
+	int wasteCount;
+	int oxygenCount;
+	int peopleCount;
+	int missingLength;
+	string teamName;
+
 	return 0;
 }
