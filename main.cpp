ad13e25b118cdf2e7cfb61d144bdd3bb599ebe3f
Mlerickson95 <mlerickson95@gmail.com>
2016-09-15 19:20:42 -0500

    Add choice 1 code.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 	int wasteCount;
 	int oxygenCount;
 	int peopleCount;
-	int missingLength;
+	int missionLength;
 	string teamName;
 	
 	cout << "----------------------\n MARS COLONY MAIN MENU\n 1. Set Mission Parameters\n 2. Run Simulation\n 3. Quit\n ----------------------" << endl;
@@ -25,7 +25,19 @@ int main()
 
 	if (choice == 1)
 	{
-		cout << "Set Mission Parameters" << endl;
+		cout << "Input Parameters." << endl;
+		cout << "A. Food grown - per day ............. ";
+		cin >> foodGrown_PerDay;
+		cout << "B. Cleanup - per person - per day.... ";
+		cin >> cleanup_PerPersonPerDay;
+		cout << "C. Starting units of food............ ";
+		cin >> foodCount;
+		cout << "D. Mission length in days............ ";
+		cin >> missionLength;
+		cout << "E. People on mission................. ";
+		cin >> peopleCount;
+		cout << "F. Team name......................... ";
+		cin >> teamName;
 	}
 	else if ( choice == 2)
 	{
