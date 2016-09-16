ba7483b53e2aef6330a2d2af233fe27015236904
ahmet2779 <asandikc@stumail.jccc.edu>
2016-09-15 19:02:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,25 @@ int main()
 	int peopleCount;
 	int missingLength;
 	string teamName;
+	
+	cout << "----------------------\n MARS COLONY MAIN MENU\n 1. Set Mission Parameters\n 2. Run Simulation\n 3. Quit\n ----------------------" << endl;
+	cout << "Choice >>" << endl;
+	int choice;
+	cin >> choice;
+
+	if (choice == 1)
+	{
+		cout << "Set Mission Parameters" << endl;
+	}
+	else if ( choice == 2)
+	{
+		cout << "Run Simulation" << endl;
+	}
+	else if (choice == 3)
+	{
+		done = true;
+	}
+
 
 	return 0;
 }
